f122cffa2abad0e1ed6c09b8f11950986794afd2
ardenaAfif <aafifpratama@gmail.com>
2023-07-09 19:24:25 +0700

    added fragment for home, profile, rekap and maps

--- app/src/main/java/com/android/abseka/ui/home/MainActivity.kt
+++ app/src/main/java/com/android/abseka/ui/home/MainActivity.kt
@@ -2,11 +2,42 @@ package com.android.abseka.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.android.abseka.R
+import com.android.abseka.databinding.ActivityMainBinding
+import com.android.abseka.ui.maps.MapsFragment
+import com.android.abseka.ui.profile.ProfileFragment
+import com.android.abseka.ui.rekap.RekapFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.apply {
+            bottomNavView.background = null
+
+            bottomNavView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> replaceFragment(HomeFragment())
+                    R.id.location -> replaceFragment(MapsFragment())
+                    R.id.rekap -> replaceFragment(RekapFragment())
+                    R.id.profile -> replaceFragment(ProfileFragment())
+                }
+                true
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
 }
--- app/src/main/java/com/android/abseka/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/android/abseka/ui/maps/MapsFragment.kt
@@ -1,43 +1,61 @@
 package com.android.abseka.ui.maps
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.android.abseka.R
+import com.android.abseka.databinding.FragmentMapsBinding
+import com.google.android.gms.location.GeofencingClient
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
 
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var geofencingClient: GeofencingClient
+
+    private val centerLat = -7.9017382
+    private val centerLng = 111.4917769
+    private val geofenceRadius = 100.0
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap = googleMap
+        mMap.uiSettings.apply {
+            isCompassEnabled = true
+            isZoomControlsEnabled = true
+            isMapToolbarEnabled = true
+        }
+
+        unidaPoint()
+        getMyLocation()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,4 +63,74 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    private fun unidaPoint() {
+        val unida =LatLng(centerLat, centerLng)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(unida, 17f))
+
+        mMap.addCircle(
+            CircleOptions()
+                .center(unida)
+                .radius(geofenceRadius)
+                .fillColor(0x22FF0000)
+                .strokeColor(Color.RED)
+                .strokeWidth(3f)
+        )
+    }
+
+    // get my location
+    private fun getMyLocation() {
+        if (checkForegroundAndBackgroundLocationPermission()) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    // Permission
+    private val requestBackgroundLocationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    private fun checkPermission(permission: String): Boolean {
+        return context?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                permission
+            )
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val runningQOrLater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+
+    @TargetApi(Build.VERSION_CODES.Q)
+    private fun checkForegroundAndBackgroundLocationPermission(): Boolean {
+        val foregroundLocationApproved = checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        val backgroundPermissionApproved =
+            if (runningQOrLater) {
+                checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            } else {
+                true
+            }
+        return foregroundLocationApproved && backgroundPermissionApproved
+    }
+
+    @TargetApi(Build.VERSION_CODES.Q)
+    private val requestLocationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {isGranted: Boolean ->
+            if (isGranted) {
+                if (runningQOrLater) {
+                    requestBackgroundLocationPermissionLauncher.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                } else {
+                    getMyLocation()
+                }
+            }
+        }
 }
